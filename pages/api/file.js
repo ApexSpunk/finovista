b6177b2ead7a03c3e7dcb234d22aa11aabaece22
Chandan <indspunk@gmail.com>
2022-09-04 09:11:09 +0530

    fileuploder

---
+++ pages/api/file.js
@@ -0,0 +1,37 @@
+import formidable from "formidable";
+import fs from "fs";
+
+export const config = {
+    api: {
+        bodyParser: false
+    }
+};
+
+const post = async (req, res) => {
+    const form = new formidable.IncomingForm();
+    console.log(form)
+    form.parse(req, async function (err, fields, files) {
+        await saveFile(files.file);
+        return res.status(201).send("");
+    });
+};
+
+const saveFile = async (file) => {
+    const data = fs.readFileSync(file.filepath);
+    fs.writeFileSync(`./public/uploads/${file.originalFilename}`, data);
+    await fs.unlinkSync(file.filepath);
+    console.log('done')
+    return;
+};
+
+export default (req, res) => {
+    req.method === "POST"
+        ? post(req, res)
+        : req.method === "PUT"
+            ? console.log("PUT")
+            : req.method === "DELETE"
+                ? console.log("DELETE")
+                : req.method === "GET"
+                    ? console.log("GET")
+                    : res.status(404).send("");
+};
