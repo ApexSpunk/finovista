6935faf1a2371796ae4f33643008bab21741c23b
Chandan <indspunk@gmail.com>
2022-10-19 15:49:01 +0530

    Css Updated

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
   const [isOpen, setIsOpen] = React.useState(false);
 
   return (
-    <nav className=" sticky top-0">
+    <nav className=" sticky top-0 z-10" >
       <div className="nav-wrapper">
         <div className="banner">
           <p>
