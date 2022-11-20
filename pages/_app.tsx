d6cd5cd23ecda14cd730871d0952c038e21501d9
Chandan <indspunk@gmail.com>
2022-11-20 10:20:18 +0530

    Updated Tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,15 @@ import '../styles/main.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react"
 import NextNProgress from 'nextjs-progressbar';
+import ReactGA from 'react-ga';
+import { useEffect } from 'react';
+const TRACKING_ID = "UA-106154633-1"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <>
       <NextNProgress color="#29D" startPosition={0.3} stopDelayMs={1000} height={3} showOnShallow={true} />
