11608f5542ccaaa0e7f1b4dc0a178154199b211b
Chandan <indspunk@gmail.com>
2022-12-06 19:06:49 +0530

    Socials Updated

--- components/Utils/Socials.js
+++ components/Utils/Socials.js
@@ -11,6 +11,7 @@ function Socials(props) {
                         <a href="https://www.instagram.com/finovista/" target="_blank"><img src="../img/instagram.png" alt="" width={width} /></a>
                         <a href="https://mobile.twitter.com/finovista" target="_blank"><img src="../img/twitter.png" alt="" width={width} /></a>
                         <a href="https://www.linkedin.com/company/finovista/" target="_blank"><img src="../img/linkedin.png" alt="" width={width} /></a>
+                        <a href="https://www.youtube.com/@finovista" target="_blank"><img src="../youtube.png" alt="" width={width} /></a>
                     </div>
                 </div>
             </div>
