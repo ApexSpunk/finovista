29bf2e8769d2fd85150dd4c4a060e4f5ee864707
Chandan <indspunk@gmail.com>
2022-12-09 16:50:25 +0530

    Updated

---
+++ pages/careers.js
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+import React, { useEffect } from 'react'
+import Footer from '../components/Utils/Footer'
+import Navbar from '../components/Utils/Navbar'
+
+function careers() {
+  return (
+    <div>
+        <Head>
+            <title>Careers | Finovista</title>
+            <link rel="icon" href="/favicon.ico" />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+        </Head>
+        <div>
+            <Navbar />
+            <div className='mb-40'>
+                <div className='text-center mt-12'>
+                    <p className='font-bold text-blue-700 mt-2'>\ Kickstart your career with us \</p>
+                    <p className='text-3xl mt-4 font-semibold'>Careers</p>
+                </div>
+                <div className='grid grid-cols-2 md:grid-cols-4 gap-12 my-10 mx-4 md:mx-24'>
+                
+                </div>
+            </div>
+            <Footer />
+        </div>
+    </div>
+)
+}
+
+export default careers
